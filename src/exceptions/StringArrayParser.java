34369886eb0a342b2551f43818fc4784fd3ce23f
StringManu <manuel.scheiber@hak-reutte.ac.at>
2024-05-21 08:10:22 +0200

    New assignment Exceptions

---
+++ src/exceptions/StringArrayParser.java
@@ -0,0 +1,26 @@
+package exceptions;
+
+public class StringArrayParser {
+	
+	private static int parseStringToInt(String string, int defaultValue) {
+		try {
+			return Integer.parseInt(string);
+		}	catch(NumberFormatException e) {
+			return defaultValue;
+		}
+	}
+	
+	public static int[] parseInts(String... numbers) {
+		int[] intNumbers = new int[numbers.length];
+		for(int i=0;i<numbers.length;i++) {
+			intNumbers[i] = parseStringToInt(numbers[i], 0);
+		}
+		return intNumbers;
+	}
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
--- src/string_Stringbuilder/SumOfTheDigits.java
+++ src/string_Stringbuilder/SumOfTheDigits.java
@@ -1,10 +1,18 @@
 package string_Stringbuilder;
 
 public class SumOfTheDigits {
-
+	
+	public static long digitSum(long value) {
+		long ergebnis=0;
+		String valueAlsString = Long.toString(value);
+		for(int i=0;i<valueAlsString.toCharArray().length;i++) {
+			ergebnis += Character.getNumericValue(valueAlsString.toCharArray()[i]);
+		} return ergebnis;
+	}
+	
 	public static void main(String[] args) {
-		
-
+		long hallo=15357892;
+		System.out.println(digitSum(hallo));
 	}
 
 }
